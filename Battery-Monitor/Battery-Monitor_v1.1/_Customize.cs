4f1e9493228ef1f1980713320bcad8032be04d58
ThrowawAPI <throwawAPI+github@gmail.com>
2020-04-05 22:19:36 -0400

    ONLY_SAME_CONSTRUCT_BATTERIES option removed for performance reasons Fix #1

---
+++ Battery-Monitor/Battery-Monitor_v1.1/_Customize.cs
@@ -0,0 +1,11 @@
+//
+// VARIABLES (EDIT THESE)
+//
+public const string VERSION = "Battery Monitor v1.0",
+  BATTERY_STRING      = ".batteries",
+  PANEL_HEADER_STRING = "     Main Base Power Monitor";
+public const UpdateFrequency FREQ = UpdateFrequency.Update100;
+public const int PANEL_STD_ROW_COUNT = 23,
+  PANEL_WIDE_ROW_COUNT   = 23,
+  PANEL_HEADER_ROW_COUNT = 7,
+  PANEL_FOOTER_ROW_COUNT = 10;
