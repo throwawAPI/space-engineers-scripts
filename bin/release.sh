b70b232a52b2a5b84bc636ed320179b537530a35
ThrowawAPI <throwawAPI+github@gmail.com>
2020-04-23 20:54:01 -0400

    bin/release.sh works with shallow directories now

--- bin/release.sh
+++ bin/release.sh
@@ -1,8 +1,9 @@
-echo "Creating directory for release: $1"
-mkdir -p releases/$1
-echo "Copying files in $1"
-cp $1/Script.cs releases/$1/Script.cs
-cp $1/thumb.png releases/$1/thumb.png
-echo "Creating ${1%/}.zip"
-cd releases/$1/..
-zip -r $(basename $1).zip $(basename $1)
+RC_DIR="${1%/}_$2"
+echo "Creating directory for release: $RC_DIR"
+mkdir -p releases/$RC_DIR
+echo "Copying files from $1"
+cp $1/Script.cs releases/$RC_DIR/Script.cs
+cp $1/thumb.png releases/$RC_DIR/thumb.png
+echo "Creating $RC_DIR.zip"
+cd releases/$RC_DIR/..
+zip -r $(basename $RC_DIR).zip $(basename $RC_DIR)
